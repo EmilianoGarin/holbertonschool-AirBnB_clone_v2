34a2d9eb96e3a339ee7533f81b04639735b29f36
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-08-15 14:48:14 -0700

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Task 2 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Hello HBNB """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_with_text(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+if __name__ == '__main__':
+    """ start the web app """
+    app.run(host='0.0.0.0', port=5000)
